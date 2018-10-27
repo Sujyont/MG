9574da922acd779a68acca032ec055c327464b4a
Sujyont <Sujyont@gmail.com>
2018-10-27 20:01:22 +0530

    Third Commit

--- MXP/src/Program1.java
+++ MXP/src/Program1.java
@@ -4,7 +4,7 @@ public class Program1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("PQR");
+		System.out.println("PQR  and MXV");
 		System.out.println("TYU");
 	}
 
